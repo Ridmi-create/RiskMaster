8e0c8dbd756400642616d90f56d83f4ee98c253b
Ridmi <ridmi1999212@gmail.com>
2023-05-20 10:25:18 +0530

    merged

--- frontend/src/views/Dashboard.js
+++ frontend/src/views/Dashboard.js
@@ -1,11 +1,17 @@
 import React from "react";
 import Header from "../components/Header";
+import Sidebar from "../components/SideBar";
+import '../components/Sidebar.css';
 
 
 function Dashboard() {
     return(
-        <Header/>  
-    )
+     <div>
+      <Header />
+      <Sidebar />  
+      
+    </div>
+    )  
 }
 
 export default Dashboard;
