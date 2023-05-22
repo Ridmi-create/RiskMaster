3fdd201d288a489f743d7c86d28ece8322d368ed
Ridmi <ridmi1999212@gmail.com>
2023-05-22 10:31:34 +0530

    Work on Login

--- argon-dashboard-react-master/src/views/examples/Login.js
+++ argon-dashboard-react-master/src/views/examples/Login.js
@@ -1,22 +1,3 @@
-/*!
-
-=========================================================
-* Argon Dashboard React - v1.2.2
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/argon-dashboard-react
-* Copyright 2022 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/argon-dashboard-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
-
-// reactstrap components
 import {
   Button,
   Card,
@@ -31,18 +12,70 @@ import {
   Row,
   Col
 } from "reactstrap";
+import axios from 'axios';
+import { useHistory } from 'react-router-dom';
+import React, { useState } from "react";
 
 const Login = () => {
+  const [userID, setUserID] = useState("");
+  const [userPassword, setUserPassword] = useState("");
+  const [userType, setUserType] = useState("");
+  const [error, setError] = useState('');
+  const history = useHistory();
+
+  const handleUserIDChange = (e) => {
+    setUserID(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setUserPassword(e.target.value);
+  };
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    if (userType === "Admin") {
+      try {
+        const adminID = userID;
+        const adminPwd = userPassword;
+        const newLog = {
+          adminID,
+          adminPwd
+        };
+        console.log(newLog);
+
+        const response = await axios.post("http://localhost:8070/Admin/login", newLog);
+
+        if (response && response.data && response.data.token) {
+          const { token } = response.data;
+
+          // Store the token in local storage or state for future requests
+          // Example: localStorage.setItem('token', token);
+
+          // Redirect the user to the dashboard or protected route
+          history.push('/admin/index');
+        } else {
+          setError('Invalid response format');
+        }
+        
+
+      } catch (e) {
+        setError(e.response.data.message);
+        alert(error);
+      }
+      
+    }
+  }
   return (
     <>
       <Col lg="5" md="7">
         <Card className="bg-secondary shadow border-0">
           <CardHeader className="bg-transparent pb-5"></CardHeader>
-          <CardBody>  
+          <CardBody>
             <div className="text-center text-muted mb-4">
-            <h1 className="text-black">Sign in!</h1>
+              <h1 className="text-black">Sign in!</h1>
             </div>
-            <Form role="form">
+            <Form role="form" onSubmit={handleSubmit}>
               <FormGroup className="mb-3">
                 <InputGroup className="input-group-alternative">
                   <InputGroupAddon addonType="prepend">
@@ -54,6 +87,7 @@ const Login = () => {
                     placeholder="UserID"
                     type="String"
                     autoComplete="001"
+                    value={userID} onChange={handleUserIDChange}
                   />
                 </InputGroup>
               </FormGroup>
@@ -67,10 +101,30 @@ const Login = () => {
                   <Input
                     placeholder="Password"
                     type="password"
-                    autoComplete="new-password"
+                    autoComplete="new-password"                    
+                    value={userPassword} onChange={handlePasswordChange}
                   />
                 </InputGroup>
               </FormGroup>
+              <FormGroup>
+                <InputGroup className="input-group-alternative">
+                  <InputGroupAddon addonType="prepend">
+                    <InputGroupText>
+                      <i className="ni ni-lock-circle-open" />
+                    </InputGroupText>
+                  </InputGroupAddon>
+                  <select className="form-control mt-1"
+                    onChange={(e) => {
+                      setUserType(e.target.value);
+                    }}>
+                    <option selected>Select User Type</option>
+                    <option value="Admin">Admin</option>
+                    <option value="RiskOwner">Risk Owner</option>
+                    <option value="RiskGovernance">Risk Governance</option>
+                  </select>
+                </InputGroup>
+              </FormGroup>
+
               <div className="custom-control custom-control-alternative custom-checkbox">
                 <input
                   className="custom-control-input"
@@ -85,7 +139,7 @@ const Login = () => {
                 </label>
               </div>
               <div className="text-center">
-                <Button className="my-4" color="primary" type="button">
+                <Button className="my-4" color="primary" type="submit">
                   Sign in
                 </Button>
               </div>
@@ -102,7 +156,7 @@ const Login = () => {
               <small>Forgot password?</small>
             </a>
           </Col>
-          
+
         </Row>
       </Col>
     </>
